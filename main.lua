051d39f44cc7dbb6c5fb64481b538b351587b6c0
Gavin Massey <mdk@mystacktrace.org>
2016-06-11 23:35:48 +0100

    Added HC library for collision detection

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
+package.path = package.path .. ";./?/init.lua"
 require("bot")
-
+HC = require("HC")
+shapes = require("HC.shapes")
 local screen_w = 0
 local screen_h = 0
 
@@ -7,6 +9,7 @@ local lg = love.graphics
 local font14 = nil
 
 local bot = bot.create(50, 50, 0, 50, 255, 50, 255)
+local colliding = false
 
 function love.load()
     -- Setting up a nice font
@@ -15,8 +18,9 @@ function love.load()
 
     screen_w = love.graphics.getWidth( )
     screen_h = love.graphics.getHeight( )
-
-
+	mouse = HC.circle(400, 300, 20)
+	mouse:moveTo(love.mouse.getPosition())
+	foo = HC.circle(400, 300, 30)
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -28,12 +32,20 @@ function love.keyreleased(key, scancode)
 
 end
 
+function love.update(dt)
+	colliding = false
+	mouse:moveTo(love.mouse.getPosition())
+
+	for shape, delta in pairs(HC.collisions(mouse)) do
+		colliding = true
+		break
+	end
+end
+
 function love.mousemoved( x, y, dx, dy )
 end
 
 function love.draw()
-
-
     lg.push()
 
         lg.setColor(255,100,100,150)
@@ -46,7 +58,12 @@ function love.draw()
         lg.pop()
 
         bot:draw()
-
+		foo:draw('fill')
+		if colliding then
+			mouse:draw('line')
+		else
+			mouse:draw('fill')
+		end
     lg.pop()
 
 
