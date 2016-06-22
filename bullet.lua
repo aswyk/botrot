61791bc5eecdd3d05a60f9b6b0d311c280edeab1
Andre Schalkwyk <Andre.Schalkwyk@emc.com>
2016-06-22 13:50:32 -0400

    Doubled bullet speed
    The original bullet speed value was 50. Increasing it to 100 will make avoiding it very hard.

--- bullet.lua
+++ bullet.lua
@@ -30,7 +30,8 @@ function bullet.create(x, y, ang, r, g, b, a)
     --self.m_scale = 0.25
     self.m_scale = 0.25
 
-    self.m_bulletSpeedUnScaled = 50;
+    -- Original m_bulletSpeedUnScaled value = 50
+    self.m_bulletSpeedUnScaled = 100;
     self.m_bulletSpeed = self.m_bulletSpeedUnScaled * self.m_scale;
     self.m_xVel = 0
     self.m_yVel = 0
