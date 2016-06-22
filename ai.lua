b0b4ed6688082b8ba18ca56f35a558c538dfcc6b
Andre Schalkwyk <Andre.Schalkwyk@emc.com>
2016-06-22 13:55:52 -0400

    Adding ai.lua
    This adds the AI base class. Each implimentation of an AI will inherit off of this and impliment onThink.

---
+++ ai.lua
@@ -0,0 +1,19 @@
+AI = {}--{ prompt = "$USER$ on $MACHINE$ in $PATH$"}
+AI.__index = AI
+
+
+local lg = love.graphics
+local lk = love.keyboard
+
+-- AI Constructor
+function AI.create(name)
+    local self = setmetatable({}, AI)
+
+    self.m_name = name
+    return self
+end
+
+-- AI onThink function. Each AI will override this function.
+function AI:onThink()
+
+end
